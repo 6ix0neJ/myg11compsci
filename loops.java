254a7372dfd5f3a1d946e54f247f642948d4ec63
Jibril Richardson <73800122+6ix0neJ@users.noreply.github.com>
2025-09-23 10:58:51 -0400

    Add files via upload
    Worked on loops today

---
+++ loops.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class loops {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int i = 0;
+        while(i < 10) {
+            System.out.println(i + " loops!");
+            i = i + 1;
+        }
+        System.out.println(" ");
+
+        /*
+        *for loop syntax remind*
+        for (initial value; condition; counting variable)
+         */
+
+        for (int j = 0; j <= 10; j++) {
+            System.out.println(j + " Cycles Completed");
+        }
+        System.out.println("END");
+    }
+}
