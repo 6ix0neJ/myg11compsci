4c6c445eb4053ffd08d29370f1ff7db2be0c2e26
Jibril Richardson <73800122+6ix0neJ@users.noreply.github.com>
2025-09-23 21:42:18 -0400

    Another assignment from my teacher
    Assignment description from my teacher:
    
    Write a Java program that:
    
    Prompts the user for an integer n.
    
    Uses a loop to calculate the sum of all integers from 1 to n (inclusive).
    
    Prints the result.
    
    Example Run
    Sample Input
    
     
    Enter a number: 5
    Sample Output
    
     
    The sum from 1 to 5 is 15
     
    
    Requirements
    Use Scanner for input.
    
    Use a loop (for or while) to do the calculation.
    
    Don’t use mathematical shortcuts like formulas (n*(n+1)/2) — the goal is to practice loops.
    
    Extra Challenge (Optional)
    Modify your program so it also prints each step of the addition:
    
     
    1 + 2 + 3 + 4 + 5 = 15

---
+++ sumfrom1n.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class sumfrom1n {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number more than 1:");
+        int n = sc.nextInt();
+
+        long j = 0;
+        for (long i = 0; i <= n; i++) {
+            j = i + j;
+            System.out.println(i + " Added to final value. Final value is now: " + j);
+        }
+
+        System.out.println(" ");
+        System.out.println("Final value is " + j);
+
+        sc.close();
+    }
+}
