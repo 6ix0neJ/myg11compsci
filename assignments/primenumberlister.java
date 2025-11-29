140b32eb7f76cc0dbc7e081fa77e8f871ab27f1f
Jibril Richardson <73800122+6ix0neJ@users.noreply.github.com>
2025-11-29 12:22:40 -0500

    Uploaded a whole rework of this program
    Updated the program to process a user-defined count of numbers and check for primality.

--- assignments/primenumberlister.java
+++ assignments/primenumberlister.java
@@ -1,26 +1,51 @@
-/*
-This program is meant to list out all
-prime numbers within a range set by the user.
-This was an assignment from my grade 11 computer
-science class.
- */
-
 import java.util.Scanner;
 
 public class primenumberlister {
-    public static void main (String[] args) {
+
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("What range of primes would you like listed?");
-        System.out.print("Enter Range Start: ");
-        int rangeS = scanner.nextInt();
-        System.out.print("Enter Range End: ");
-        int rangeE = scanner.nextInt();
 
-        findPrimes(rangeS, rangeE);
+        System.out.print("How many numbers would you like to process? ");
+        int count = scanner.nextInt();
+        int[] numbers = new int[count];
 
+        for (int i = 0; i < count; i++) {
+            System.out.print("Enter number " + (i + 1) + ": ");
+            numbers[i] = scanner.nextInt();
+        }
+        System.out.println();
+
+        int primeCount = 0;
+        System.out.println("Results:");
+        for (int num : numbers) {
+            if (isPrime(num)) {
+                System.out.println(num + " is prime.");
+                primeCount++;
+            } else {
+                System.out.println(num + " is not prime.");
+            }
+        }
+
+
+        System.out.println("\nTotal primes entered: " + primeCount);
+        System.out.print("Prime numbers: ");
+        for (int num : numbers) {
+            if (isPrime(num)) {
+                System.out.print(num + " ");
+            }
+        }
         System.out.println();
     }
-    public static void findPrimes (int a, int b) {
 
+    public static boolean isPrime(int n) {
+        if (n <= 1) return false;
+
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
